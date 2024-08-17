e787ac9e54c0e5c1ba8c04457d19841ae931bb76
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2024-08-17 18:11:31 -0300

    primera version de la web

---
+++ assets/js/scripts.js
@@ -0,0 +1,141 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Mantener la referencia a la bandera de generación de botones
+  let buttonsGenerated = false;
+
+  // Función para generar los botones
+  function generateProductionButtons() {
+    const productionsButtonsContainer = document.getElementById('productions-buttons-container');
+    productionsButtonsContainer.innerHTML = ''; // Limpiar los botones anteriores
+
+    const buttonData = [
+      { name: 'Maizena', url: 'https://open.spotify.com/artist/42HFQJwZtGRwxoVlDwgdgK' },
+      { name: 'Nubila', url: 'https://linktr.ee/nubila' },
+      { name: 'Lara Fernandez', url: 'https://open.spotify.com/artist/691Dk9GKS8rAgDUpfkMwPS' },
+      { name: 'Mateo Genca', url: 'https://open.spotify.com/artist/52zNpUR2AAV4ER89riTHt0' },
+      { name: 'Psieledas', url: 'https://psieledas.bandcamp.com/' },
+      { name: 'Irmanas', url: 'https://www.youtube.com/@Irmanas-wi9zg' },
+      { name: 'Mott', url: 'https://open.spotify.com/artist/5PvNVYs8antCfkOMeESvJu' },
+      { name: 'Yen-ji', url: 'https://open.spotify.com/artist/1b007dZ0opo4WIJK54RYLz' },
+      { name: 'Hechizo Animal', url: 'https://open.spotify.com/artist/2VMcktUnSTqXIBUIUtrW2M' },
+      { name: 'Lava Andina', url: 'https://open.spotify.com/artist/6mIYlnubsoAHVnThMC7CyY' }
+    ];
+
+    function shuffle(array) {
+      let currentIndex = array.length, randomIndex;
+      while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+      }
+      return array;
+    }
+
+    const shuffledButtons = shuffle(buttonData);
+    const containerWidth = productionsButtonsContainer.clientWidth;
+    const containerHeight = productionsButtonsContainer.clientHeight;
+
+    shuffledButtons.forEach(button => {
+      const btn = document.createElement('a');
+      btn.href = button.url;
+      btn.className = 'btn-produccion';
+      btn.textContent = button.name;
+      btn.target = '_blank';
+
+      const btnWidth = 100;
+      const btnHeight = 40;
+
+      const x = Math.random() * (containerWidth - btnWidth);
+      const y = Math.random() * (containerHeight - btnHeight);
+
+      btn.style.left = `${x}px`;
+      btn.style.top = `${y}px`;
+
+      productionsButtonsContainer.appendChild(btn);
+    });
+  }
+
+  // Listener para el botón de recarga
+  const reloadButton = document.getElementById('reloadButtons');
+  if (reloadButton) {
+    reloadButton.addEventListener('click', function () {
+      generateProductionButtons();
+    });
+  }
+
+  // Código existente para manejar la generación inicial de los botones
+  const headers = document.querySelectorAll(".accordion-header");
+
+  headers.forEach((header) => {
+    header.addEventListener("click", function () {
+      const content = this.nextElementSibling;
+      const isVisible = content.style.display === "block";
+
+      content.style.display = isVisible ? "none" : "block";
+
+      if (
+        this.parentElement.id === "producciones" &&
+        !isVisible &&
+        !buttonsGenerated
+      ) {
+        generateProductionButtons();
+        buttonsGenerated = true;
+      }
+    });
+  });
+
+  // Código existente para la traducción
+  const languageSelector = document.getElementById("languageSelector");
+
+  if (languageSelector) {
+    languageSelector.addEventListener("change", function () {
+      const selectedLanguage = this.value;
+      translatePage(selectedLanguage);
+    });
+  }
+
+  function translatePage(language) {
+    const elementsToTranslate = document.querySelectorAll("i18n-key");
+    elementsToTranslate.forEach((element) => {
+      const translationKey = element.getAttribute("key");
+      const keys = translationKey.split(".");
+      let translation = translations[language];
+
+      keys.forEach((key) => {
+        translation = translation[key];
+      });
+
+      if (translation) {
+        element.innerHTML = translation;
+      } else {
+        console.warn(
+          `Missing translation for ${translationKey} in ${language}`
+        );
+      }
+    });
+
+    const elementsWithAlt = document.querySelectorAll("[data-i18n-alt]");
+    elementsWithAlt.forEach((element) => {
+      const altKey = element.getAttribute("data-i18n-alt");
+      const keys = altKey.split(".");
+      let translation = translations[language];
+
+      keys.forEach((key) => {
+        translation = translation[key];
+      });
+
+      if (translation) {
+        element.setAttribute("alt", translation);
+      } else {
+        console.warn(`Missing alt translation for ${altKey} in ${language}`);
+      }
+    });
+
+    const titleElement = document.querySelector('title[data-i18n="title"]');
+    if (titleElement) {
+      titleElement.innerText = translations[language].title;
+    }
+  }
+
+  const initialLanguage = languageSelector ? languageSelector.value : "es";
+  translatePage(initialLanguage);
+});
---
+++ assets/js/translations.js
@@ -0,0 +1,76 @@
+const translations = {
+  es: {
+    title: "La Casa Mutante",
+    videoFallback: "Tu navegador no soporta la etiqueta de video.",
+    aboutUs: {
+      header: "Sobre La Casa",
+      paragraph1:
+        "La Casa Mutante es un espacio de producción artística que nació en el barrio Jardín de Córdoba, Argentina. Somos una comunidad creativa y colaborativa que fomenta la producción cultural independiente. La colaboración y la autogestión son nuestros principios.",
+      paragraph2:
+        "<strong>Nuestra Historia:</strong> La Casa Mutante surge hace diez años de la necesidad de abrir un espacio donde podamos crear con otrxs, en el que las ocurrencias y pensamientos - que a veces parecen poco importantes - puedan florecer libremente. Nuestro propósito es alimentar la creatividad y darle un sustento para su realización, inspirar la producción artística y promover un entorno donde las ganas de crear en grupo sean la fuerza motriz. Desde nuestros inicios, hemos producido numerosos videos, discos y podcasts, trabajando colectivamente para aportar al arte cordobés.",
+      paragraph3:
+        "<strong>Nuestro Equipo:</strong> Somos un equipo diverso y en constante evolución, formado por personas de distintxs orígenes y amores: plásticos, musicales, visuales, poéticos, tecnológicos, científicos, políticos. Nos unen las ganas de hacer y la confianza en un flujo común y colectivo. Cada miembro aporta su energía y visión, haciendo de La Casa Mutante un lugar de creación continua.",
+      paragraph4:
+        "<strong>Valores y Objetivos:</strong> Nos negamos a encasillarnos en categorías rígidas y buscamos que múltiples proyectos coexistan en armonía. Creemos en crear en conjunto y en que la elaboración artística siempre es integral. Hemos creado demasiadas separaciones que diluyen el sentido de hacer. No nos interesan las medallas, ni los laureles. Valoramos lo no racional y confiamos en la sincronía colectiva como una poética que guía, ofrece temáticas, organiza política. Queremos crear un paréntesis de tiempo y espacio donde la creatividad y la inspiración se alimenten mutuamente.",
+    },
+    productions: {
+      header: "Producciones, Proyectos y Artistas",
+      img1: "Producción 1",
+      img2: "Producción 2",
+      img3: "Producción 3",
+    },
+    participate: {
+      header: "Participa",
+      subtitle: "¿Cómo podés sumar al Proyecto?",
+      how1: "<strong>Compartiendo tus obras:</strong> Comparte tu producción artística o musical con nosotros. Nos gustaría circular y hacer caminos conjuntos para que más personas puedan disfrutar del arte. Estamos dispuestxs a encontrar nuevas formas de colaborar entre artistas y obras.",
+      how2: "<strong>Con tu fuerza trabajo:</strong> Unite a nuestro equipo. Si te interesa el trabajo audiovisual y los espectáculos musicales, puedes colaborar en áreas como vestuario, escenografía, iluminación, postproducción, sonido, y cámara. <a href='https://www.instagram.com/la.casa.mutante/' target='_blank'>Inscríbete aquí</a>.",
+      how3: "<strong>Con una contribución económica:</strong> Apoya el crecimiento de La Casa Mutante. Puedes hacer una donación a través de nuestra alcancía virtual y ayudar a sostener nuestras actividades y proyectos. <a href='https://www.instagram.com/la.casa.mutante/' target='_blank'>Contribuye aquí</a>.",
+    },
+    contact: {
+      header: "Contacto",
+      social:
+        "Redes Sociales: Síguenos en nuestras redes sociales para mantenerte al día con las novedades y eventos de La Casa Mutante. <a href='https://www.instagram.com/la.casa.mutante/' target='_blank'>Instagram</a> & <a href='https://www.youtube.com/@lacasamutante7513' target='_blank'>YouTube</a>",
+      location: "Ubicación: Bº Jardín, Córdoba, Argentina.",
+    },
+    footer:
+      "© 2024 La Casa Mutante. Distribuido bajo la licencia <a href='https://opensource.org/licenses/GPL-3.0' target='_blank'>GPL-3.0</a>. ¡Compártelo y contribuye!",
+  },
+  en: {
+    title: "La Casa Mutante",
+    videoFallback: "Your browser does not support the video tag.",
+    aboutUs: {
+      header: "About The House",
+      paragraph1:
+        "La Casa Mutante is an artistic production space born in the Jardín neighborhood of Córdoba, Argentina. We are a creative and collaborative community that promotes independent cultural production. Collaboration and self-management are our principles.",
+      paragraph2:
+        "<strong>Our History:</strong> La Casa Mutante emerged ten years ago from the need to open a space where we can create with others, where occurrences and thoughts - which sometimes seem unimportant - can flourish freely. Our purpose is to nurture creativity and give it support for its realization, to inspire artistic production and promote an environment where the desire to create as a group is the driving force. Since our beginnings, we have produced numerous videos, records, and podcasts, working collectively to contribute to the Cordoban art.",
+      paragraph3:
+        "<strong>Our Team:</strong> We are a diverse and constantly evolving team, made up of people from different origins and loves: plastic, musical, visual, poetic, technological, scientific, political. We are united by the desire to do and the trust in a common and collective flow. Each member contributes their energy and vision, making La Casa Mutante a place of continuous creation.",
+      paragraph4:
+        "<strong>Values and Objectives:</strong> We refuse to pigeonhole ourselves into rigid categories and seek for multiple projects to coexist in harmony. We believe in creating together and that artistic elaboration is always integral. We have created too many separations that dilute the sense of doing. We are not interested in medals or laurels. We value the non-rational and trust in collective synchronicity as a poetics that guides, offers themes, organizes politics. We want to create a parenthesis of time and space where creativity and inspiration mutually nourish each other.",
+    },
+    productions: {
+      header: "Productions, Projects and Artists",
+      img1: "Production 1",
+      img2: "Production 2",
+      img3: "Production 3",
+    },
+    participate: {
+      header: "Participate",
+      subtitle: "How can you contribute to the Project?",
+      how1: "<strong>Sharing your works:</strong> Share your artistic or musical creations with us. We aim to circulate and forge joint paths so that more people can enjoy the art. We are open to discovering new ways to collaborate between artists and their works.",
+      how2: "<strong>With your work:</strong> Join our team. If you are interested in audiovisual work and musical performances, you can help in areas such as costumes, set design, lighting, post-production, sound, and camera work. Sign up here <a href='https://www.instagram.com/la.casa.mutante/' target='_blank'>Registration Form</a>.",
+      how3: "<strong>With a financial contribution:</strong> Support the growth of La Casa Mutante. You can make a donation through our virtual piggy bank to help sustain our activities and projects. <a href='https://www.instagram.com/la.casa.mutante/' target='_blank'>Contribute here</a>.",
+    },
+    contact: {
+      header: "Contact",
+      social:
+        "Social Media: Follow us on our social networks to stay up to date with the latest news and events from La Casa Mutante. <a href='https://www.instagram.com/la.casa.mutante/' target='_blank'>Instagram</a> & <a href='https://www.youtube.com/@lacasamutante7513' target='_blank'>YouTube</a>",
+      location: "Location: Jardín neighborhood, Córdoba, Argentina.",
+    },
+    footer:
+      "© 2024 La Casa Mutante. Distributed under the <a href='https://opensource.org/licenses/GPL-3.0' target='_blank'>GPL-3.0</a> license. Share and contribute!",
+  },
+};
+
+window.translations = translations;
